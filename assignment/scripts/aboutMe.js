053eecf0691c06e8c9c058d2c5176a2c4542e469
Teairra Craig <craigteairra@gmail.com>
2024-11-11 10:16:50 -0600

    git commit -m"canges in js"

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,7 +1,10 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Teairra';
+console.log('my first name');
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+let lastname = 'Craig';
+console.log(lastname);
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
